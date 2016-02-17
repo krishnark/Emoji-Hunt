14d89610872f59f4f1a590bbad9169f0d7bedc1b
krishnark <krishnarkothari@gmail.com>
2016-02-17 13:23:56 -0800

    Adding the complete successful implementation of the project

--- Matching_Game.js
+++ Matching_Game.js
@@ -1,8 +1,17 @@
 var numberOfFaces = 5;
 var theLeftSide = document.getElementById("leftSide");
 var theRightSide = document.getElementById("rightSide");
+var theBody = document.getElementsByTagName("body")[0];
 
 function generateFaces(){
+
+	while (theLeftSide.firstChild) {
+        theLeftSide.removeChild(theLeftSide.firstChild);
+    }
+    
+    while (theRightSide.firstChild) {
+    	theRightSide.removeChild(theRightSide.firstChild);
+    }
 
 	for (var i = 0; i < numberOfFaces; i++) {
 		image = document.createElement("img");
@@ -15,4 +24,20 @@ function generateFaces(){
 	leftSideImages = theLeftSide.cloneNode(true); // true is an arguement to copy an entire branch
 	leftSideImages.removeChild(leftSideImages.lastChild);
 	theRightSide.appendChild(leftSideImages);
+
+	theLeftSide.lastChild.onclick = function nextLevel(event){
+
+		console.log("I am inside the click event");
+		event.stopPropagation();
+		numberOfFaces += 5;
+		generateFaces();
+	}
+
+	theBody.onclick = function gameOver() {
+		
+		console.log("I am inside the click event");
+		alert("Game Over!");
+		theBody.onclick = null;
+		theLeftSide.lastChild.onclick = null;
+	}
 }
